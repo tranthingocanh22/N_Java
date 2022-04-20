51a4455c47bee7533bbb7f2d2a9e5188a3e22061
tranthingocanh22 <103102505+tranthingocanh22@users.noreply.github.com>
2022-04-20 22:46:42 +0700

    cvvv

--- Task3/P5_Classname.java
+++
@@ -1,9 +0,0 @@
-/*
-*Comment to state the purpose of the program
-*/
-public class P5_Classname { //Choose a meaningful Classname. Save as "Classname.java"
-public static void main(String[] args) { //Entry point of the program
-//Your programming statements here!!!
-    System.out.println("My name Nanh");
-    }
-}
--- Task3/P8_2_CylinderComutation.java
+++
@@ -1,2 +0,0 @@
-public class P8_2_CylinderComutation {
-}
