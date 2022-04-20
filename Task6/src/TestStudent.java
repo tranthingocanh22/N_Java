b36bfda50091a844f6bbda9f30f20a78c27a2e01
tranthingocanh22 <103102505+tranthingocanh22@users.noreply.github.com>
2022-04-20 22:13:46 +0700

    abc

--- Task6/src/TestStudent.java
+++
@@ -1,7 +0,0 @@
-public class TestStudent {
-    public static void main(String[] args) {
-        Student B1 = new Student();
-        System.out.println("Name: " +B1.getName());
-        System.out.println("Age: "+B1.getAge());
-    }
-}
